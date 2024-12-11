e23636e7f8ce868c6a3cd63a92f9dd24dbf64c07
Vĩnh Kỳyy <89082148+vinhky123@users.noreply.github.com>
2024-12-11 16:16:45 +0700

    Update leaflet.js

--- docs/js/leaflet.js
+++ docs/js/leaflet.js
@@ -1,5 +1,5 @@
 var map = L.map("map").setView([10.795376, 106.661339], 12);
-HOST = "http://3.107.51.155/";
+HOST = "http://3.107.51.155";
 
 var tileLayer = L.tileLayer(
 	"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -62,45 +62,37 @@ function loadJSONData() {
 }
 
 function handleButtonClick(name, lat, lng, marker) {
-	console.log(`Button clicked for location: ${name}`);
-
 	const globalLoader = document.getElementById("globalLoader");
 	globalLoader.style.display = "flex";
 
 	callAPI(lat, lng, (response) => {
 		globalLoader.style.display = "none";
-		console.log(`API response for location ${name}:`, response);
 
-		var content = "";
-
-		if (response > 30) {
-			content = `
-				<br>
-				Tốc độ trung bình hiện tại là: <strong>${response} km/h</strong>
-				<br>
-				<strong style="color: #169325;">Nhanh, có vẻ không kẹt lắm</strong>
-		`;
-		} else if (response <= 30) {
-			content = `
-				<br>
-				Tốc độ trung bình hiện tại là: <strong>${response} km/h</strong>
-				<br>
-				<strong style="color: #c93c3c;">Chậm, đừng đi đường này, kẹt rồi!</strong>
-		`;
+		let content = "";
+		if (response && typeof response === "number") {
+			if (response > 30) {
+				content = `
+                    <br>
+                    Tốc độ trung bình hiện tại là: <strong>${response} km/h</strong>
+                    <br>
+                    <strong style="color: #169325;">Nhanh, có vẻ không kẹt lắm</strong>
+                `;
+			} else {
+				content = `
+                    <br>
+                    Tốc độ trung bình hiện tại là: <strong>${response} km/h</strong>
+                    <br>
+                    <strong style="color: #c93c3c;">Chậm, đừng đi đường này, kẹt rồi!</strong>
+                `;
+			}
 		} else {
 			content = `
-			<br>
-			<strong style="color: #c93c3c;">Có lỗi xảy ra, vui lòng thử lại sau</strong>
-		`;
+                <br>
+                <strong style="color: #c93c3c;">Không thể lấy dữ liệu, vui lòng thử lại sau!</strong>
+            `;
 		}
 
-		marker.setPopupContent(`
-            ${name}
-            <br>
-			Tốc độ hiện tại là: <strong>${response} km/h</strong>
-			<br>
-            <strong>${alert}</strong>
-        `);
+		marker.setPopupContent(`${name} ${content}`);
 		marker.openPopup();
 	});
 }
